245f44e9b913abf517c4e02be8fd3630a46a8aa6
Jiaxiang Yi <yagafighting@gmail.com>
2023-12-06 14:32:23 +0100

    preliminary-cluster-results

--- examples/studies/mf_data_scarce_noiseless/hk/pbsjob.sh
+++ examples/studies/mf_data_scarce_noiseless/hk/pbsjob.sh
@@ -2,7 +2,7 @@
 # Torque directives (#PBS) must always be at the start of a job script!
 
 #PBS -N hk
-#PBS -q mse
+#PBS -q guest
 #PBS -l nodes=1:ppn=12
 #PBS -o out.$PBS_JOBID
 #PBS -e err.$PBS_JOBID
