39805522a94b0ce88de00a3c018592d0c024f908
JiaxiangYi96 <yagafighting@gmail.com>
2024-08-05 13:09:27 +0200

    fix typo

--- src/mfbml/problems/__init__.py
+++ src/mfbml/problems/__init__.py
@@ -1,4 +1,4 @@
 from .high_dimension_problems import (MFB1, Meng4D, Meng20D, MengCase1,
                                       Rosenbrock)
-from .illutrative_problems import mf_Forrester, mf_Forrester_torch
+from .illustrative_problems import mf_Forrester, mf_Forrester_torch
 from .low_dimension_problems import register_problem
